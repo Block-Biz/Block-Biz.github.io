ca8b852997a307e0d80084058f27574601ab316b
Block-Biz <133973815+Block-Biz@users.noreply.github.com>
2023-05-19 18:17:25 +0200

    Add files via upload

--- Javascript/current_graphs.js
+++ Javascript/current_graphs.js
@@ -1,78 +1,123 @@
-let oneMonth = [['1/12/2022', -0.011802069, 0.007180915, 16967.13281, 81.06, 1815.2, 0.030938665
-], ['2/12/2022', 0.007136986, -0.01491452, 17088.66016, 79.86, 1809.6, -0.003089828
-], ['3/12/2022', -0.010614235, 0, 16908.23633, 79.86, 1809.6, 0
-], ['4/12/2022', 0.013058842, 0, 17130.48633, 79.86, 1809.6, 0
-], ['5/12/2022', -0.009128269, -0.038679913, 16974.82617, 76.83, 1781.3, -0.015762392
-], ['6/12/2022', 0.006733036, -0.034696277, 17089.50391, 74.21, 1782.4, 0.000617336
-], ['7/12/2022', -0.014224978, -0.031205488, 16848.12695, 71.93, 1798, 0.008714165
-], ['8/12/2022', 0.0226142, -0.008797096, 17233.47461, 71.3, 1801.5, 0.001944715
-], ['9/12/2022', -0.005838371, -0.003512473, 17133.15234, 71.05, 1810.7, 0.00509386
-], ['10/12/2022', -0.000258464, 0, 17128.72461, 71.05, 1810.7, 0
-], ['11/12/2022', -0.001433197, 0, 17104.19336, 71.05, 1810.7, 0
-], ['12/12/2022', 0.005959928, 0.026527491, 17206.4375, 72.96, 1792.3, -0.0102138
-], ['13/12/2022', 0.032864789, 0.033426293, 17781.31836, 75.44, 1825.5, 0.018354211
-], ['14/12/2022', 0.001928931, 0.022284314, 17815.65039, 77.14, 1818.7, -0.003731962
-], ['15/12/2022', -0.025628381, -0.016337029, 17364.86523, 75.89, 1787.8, -0.017136146
-], ['16/12/2022', -0.042189808, -0.022655553, 16647.48438, 74.19, 1800.2, 0.006911956
-], ['17/12/2022', 0.008827572, 0, 16795.0918, 74.19, 1800.2, 0
-], ['18/12/2022', -0.002212332, 0, 16757.97656, 74.19, 1800.2, 0
-], ['19/12/2022', -0.019176452, 0.011525188, 16439.67969, 75.05, 1797.7, -0.0013897
-], ['20/12/2022', 0.027988705, 0.011525596, 16906.30469, 75.92, 1825.4, 0.015291071
-], ['21/12/2022', -0.005264509, 0.029205788, 16817.53516, 78.17, 1823.9, -0.000822076
-], ['22/12/2022', 0.000761215, -0.006288118, 16830.3418, 77.68, 1795.3, -0.015804927
-], ['23/12/2022', -0.001985808, 0.024039313, 16796.95313, 79.57, 1804.2, 0.004945141
-], ['24/12/2022', 0.003019956, 0, 16847.75586, 79.57, 1804.2, 0
-], ['25/12/2022', -0.00034251, 0, 16841.98633, 79.57, 1804.2, 0
-], ['26/12/2022', 0.004609856, 0, 16919.80469, 79.57, 1804.2, 0
-], ['27/12/2022', -0.012048247, -0.001509244, 16717.17383, 79.45, 1823.1, 0.010421069
-], ['28/12/2022', -0.00989505, -0.007073416, 16552.57227, 78.89, 1815.8, -0.004012207
-], ['29/12/2022', 0.005408645, -0.00584797, 16642.3418, 78.43, 1826, 0.00560164
-], ['30/12/2022', -0.002391696, 0.02181813, 16602.58594, 80.16, 1826.2, 0.000109523
-], ['31/12/2022', -0.003323666, 0, 16547.49609, 80.16, 1826.2, 0
-]]
+const signContract = document.getElementById("sign-contract");
+document.getElementById("navbar").style.display = "none";
+document.getElementById("the_graphs").style.display = "none";
+const home = document.getElementById("home")
+window.scroll(0, 0)
+signContract.addEventListener('click', function onClick() {
+    document.getElementById("navbar").style.display = "flex";
+    content = document.querySelector(".view-contract");
+    contentContract = document.querySelector(".contract-details");
+    document.getElementById("contract").style.display = "none";
+    window.scroll(0, 0)
+    document.getElementById("fund-details").style.color = "black"
+    const contractDate = document.getElementById("contract-date").value;
+    const partyOne = document.getElementById("party-one").value;
+    const partyTwo = document.getElementById("party-two").value;
+    const contractAmount = document.getElementById("contract-amount").value;
+    const duration = document.getElementById("duration").value;
+    const nameOne = document.getElementById("party-one").value;
+    const nameTwo = document.getElementById("party-one").value;
+    const endDate = document.getElementById("end-date").value;
+    const startDate = document.getElementById("start-date").value;
+    const product = document.getElementById("contract-product").value;
+    const signbyTwo = document.getElementById("signed-by-two").value;
+    const signDateTwo = document.getElementById("sign-date-two").value;
+    const signbyOne = document.getElementById("signed-by-one").value;
+    const signDateOne = document.getElementById("sign-date").value;
+    const percentage = document.getElementById("percentage").value;
+    let text = `<h4>Contract Amount: ${contractAmount}</h4>
+    <h4>End Date: ${endDate}</h4>
+    <h4>Signed by: ${signbyOne}</h4>`
+    contentContract.innerHTML = text;
+    let htmlSegement = `        <div class="contract-wrapper">
+    <div class="contract">
+        <div class="contract-header">
+            <h1>Contract</h1>
+        </div>
+        <div class="contract-body">
+            <div class="contract-clause">
+                <div class="clause-header">
+                    <h2>Custom contract</h2>
+                </div>
+                <p>
+                    This is a smart contract entered into on ${contractDate} between ${partyOne} and ${partyTwo} for the
+                    contractual amount of ${contractAmount} where ${partyOne} is
+                    the buyer and ${partyTwo} is the seller. the representatives
+                    of ${partyOne} and ${partyTwo} are ${nameOne} and ${nameTwo} respectively. The
+                    underlying asset of this contract is ${product}.
+                    This contract will begin on ${startDate} for the duration of
+                    ${duration} months/years and will end on ${endDate} unless otherwise terminated
+                    by the mutual agreement of both parties or as specified in this contract.
+                </p>
+            </div>
+            <div class="contract-clause">
+                <div class="clause-header">
+                    <h2>Optional Clause</h2>
+                </div>
+                <p>
+                <ul>
+                    <li>
+                        The parties may agree that the position in the digital asset may be closed if the losses
+                        reach ${percentage}%
+                    </li>
+                </ul>
+                </p>
+            </div>
+            <div class="contract-clause">
+                <div class="clause-header">
+                    <h2>Non-negotiable Terms and Conditions</h2>
+                </div>
+                <p>
+                <ol>
+                    <li> A minimum of 50% of the total contractual amount is to be invested in the digita asset.
+                    </li>
+                    <li>At any point before the date of maturity of contract, the position in the digital
+                        asset can be closed to continue with the traditional method of a forward contract
+                        for the remaining time period only if both parties agree to do so.
+                    </li>
+                    <li> If the above mentioned is not executed, the position in the digital asset will be
+                        closed automatically at the date of maturity of the contract.
+                    </li>
+                    <li>If the selling party incurs a loss, the buying party is under no obligation to share
+                        the losses
+                    </li>
+                    <li> BlockBiz will not be sharing any losses with either of the parties.
+                    </li>
+                    <li> A fee of 1% of the total contractual amount will be charged to each of the parties
+                        involved in this contract.
+                    </li>
+                    <li>In the event of any disputes or disagreements between the parties arising from this
+                        contract, both parties agree to first attempt to resolve the dispute amicably
+                        through mediation. If the dispute cannot be resolved through mediation, both
+                        parties agree to binding arbitration in accordance with the rules and regulations of
+                        BlockBiz
+                    </li>
+                    <li> In case of termination due to disputes, a fee of 5% of the total contractual amount
+                        will be charged to each of the parties involved in this contract.
+                    </li>
+                    <li> This Contract may be amended or modified at any time with the mutual agreement
+                        of both parties, and any amendments or modifications shall be in writing and
+                        signed by both parties
+                </ol>
+                </p>
+            </div>
+        </div>
+        <div class="contract-footer">
+            <div class="signed-by">
+                <div>Signed by ${signbyTwo}</div>
+                <div>Date ${signDateTwo}</div>
+            </div>
+            <div class="signed-by">
+            <div>Signed by ${signbyOne}</div>
+            <div>Date ${signDateOne}</div>
+            </div>
+        `
+    content.innerHTML = htmlSegement;
+});
 
-anychart.onDocumentReady(function () {
-    // create a data set
-    var dataSet = anychart.data.set(oneMonth);
-
-    // map the data for all series
-    var firstSeriesData = dataSet.mapAs({ x: 0, value: 1 });
-    var secondSeriesData = dataSet.mapAs({ x: 0, value: 2 });
-    var thirdSeriesData = dataSet.mapAs({ x: 0, value: 6 });
-    // create a line chart
-    var chart = anychart.line();
-    chart.title("Returns");
-    // specify where to display the chart
-    chart.container("container");
-
-    // draw the resulting chart
-    chart.draw();
-    chart.xAxis().title("December 2022");
-    //adjusting markers
-    var firstSeries = chart.line(firstSeriesData);
-    firstSeries.name("BTC");
-    var secondSeries = chart.line(secondSeriesData);
-    secondSeries.name("Crude Oil");
-    var thirdSeries = chart.line(thirdSeriesData);
-    thirdSeries.name("Gold");
-    firstSeries.hovered().markers().type("circle").size(4);
-    secondSeries.hovered().markers().type("circle").size(4);
-    thirdSeries.hovered().markers().type("circle").size(4);
-
-    chart.legend().enabled(true);
-    chart.tooltip().positionMode("point");
-    chart.tooltip().position("right").anchor("left-center").offsetX(5).offsetY(5);
-    //changing line colours
-    firstSeries.normal().stroke("#FFA500", 2.5);
-    secondSeries.normal().stroke("#1a1718", 2.5);
-    thirdSeries.normal().stroke("#FFD700", 2.5);
-    chart.background().enabled(true);
-    chart.background().fill("#FFFFFF");
-    chart
-        .title()
-        .enabled(true)
-        .useHtml(true)
-        .text(
-            '<span style="color: #000000; font-size:20px;">Returns</span>'
-        );
+home.addEventListener('click', function onClick() {
+    document.getElementById("home").style.color = "black";
+    document.getElementById("fund-details").style.color = "whitesmoke";
+    document.getElementById("view-contract").style.display = "none";
+    document.getElementById("the_graphs").style.display = "flex";
 });
